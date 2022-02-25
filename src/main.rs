5eb29fbbb6c511054951af78e23bd9f4e8ea8e90
Li Tian <carlostian927@berkeley.edu>
2022-02-25 20:39:20 +0000

    Add tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 
+use rand::{distributions::Uniform, Rng};
 
 fn main() {
-    let client_vals: [f64;10] = rand::random();
-    let mut masked_vals: [f64;10] = client_vals.clone();
-    for i in 0..10 {
-        for j in i+1..10 {
-            let masking_val: f64 = rand::random();
+    let range = Uniform::from(5..12);
+    let client_vals: Vec<i64> = rand::thread_rng().sample_iter(&range).take(100).collect();
+    let mut masked_vals: Vec<i64> = client_vals.clone();
+    for i in 0..100 {
+        for j in i+1..100 {
+            let masking_val: i64 = rand::thread_rng().gen_range(-10..10);
             masked_vals[i] += masking_val;
             masked_vals[j] -= masking_val;
         }
     }
-    let naive_aggregate: f64 = client_vals.iter().sum();
-    println!("Server Aggregate result: {:.2}", aggregate_server(&masked_vals));
+    let naive_aggregate: i64 = client_vals.iter().sum();
+    println!("Server Aggregate result: {:.2}", aggregate_server(masked_vals));
     println!("Naive Aggregate result: {:.2}", naive_aggregate);
 }
 
-fn aggregate_server(masked_vals: &[f64]) -> f64{
+fn aggregate_server(masked_vals: Vec<i64>) -> i64{
     masked_vals.iter().sum()
 }
